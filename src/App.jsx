cc16de539823ad715c4159951793039d9341c476
Niloofar Nayebi <nil_nyb@yahoo.com>
2025-08-25 23:13:14 -0400

    week 6 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import './App.css';
-import TodoList from './features/TodoList/TodoList';
-import TodoForm from './features/TodoForm';
-import { useState } from 'react';
+import './App.css'
+// eslint-disable-next-line no-unused-vars
+import TodoList from './features/TodoList/TodoList'
+// eslint-disable-next-line no-unused-vars
+import TodoForm from './features/TodoForm'
+import { useState } from 'react'
+
 
 function App() {
+  // Define the todo list
   const [todoList, setTodoList] = useState([]);
 
-  const handleAddTodo = (title) => {
-    const todo = {
+  // Function to add a new todo
+  function handleAddTodo(newTitle) {
+    const newTodo = {
       id: Date.now(),
-      title: title,
-      isCompleted: false,
+      title: newTitle,
+      isCompleted: false, 
     };
-    setTodoList([...todoList, todo]);
-  };
+    setTodoList([...todoList, newTodo]);
+  }
 
-   function completeTodo(id){
-    const updateTodos = todoList.map((todo)=>{
-      if(todo.id===id){
-       return {...todo , isCompleted:true};
+  function completeTodo(id) {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: true };
       }
       return todo;
-    })
-    setTodoList(updateTodos);
-   }
+    });
   
+    setTodoList(updatedTodos);
+  }
+
+  function updateTodo(editedTodo) {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === editedTodo.id) {
+        return { ...todo, title: editedTodo.title };
+      }
+      return todo;
+    });
+  
+    setTodoList(updatedTodos);
+  }
+  
+  
+  
+
   return (
     <div>
-      <h1>My Todos</h1>
+      <h1>Todo List</h1>
       <TodoForm onAddTodo={handleAddTodo} />
-      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} onUpdateTodo={updateTodo} />
     </div>
   );
 }
--- src/features/TodoForm.jsx
+++ src/features/TodoForm.jsx
@@ -1,45 +1,34 @@
-import { useRef } from "react";
-import { useState } from "react";
+import TextInputWithLabel from "../shared/TextInputWithLabel";
+
+import { useState, useRef } from "react";
+
 
 function TodoForm({ onAddTodo }) {
-   const [workingTodoTitle, setWorkingTodoTitle] = useState("");
+  const [workingTodoTitle, setWorkingTodoTitle] = useState("");
+  const inputRef = useRef();
 
-   const todoTitleInput=useRef(null);
-//    const todoTitleInput = useRef(null); // null is better than "" for DOM elements
-
-    // function handleAddTodo(event){
-    //     event.preventDefault()
-    //     //  console.dir(event.target.title.value)
-    //     // console.dir(event.target.title)
-    //     const title=event.target.title.value // Get the input value
-    //     onAddTodo(title); // Call the function passed from parent with the input value
-  
-    //   event.target.title.value = ""; // Clear the input after submission
-    //   todoTitleInput.current.focus();
-
-    // }
-    function handleAddTodo(event) {
+  function handleAddTodo(event) {
     event.preventDefault();
-    onAddTodo(workingTodoTitle);
-    setWorkingTodoTitle("");  // reset input
-    todoTitleInput.current.focus();
+    const title = workingTodoTitle.trim();
+
+    if (title !== "") {
+      onAddTodo(title);
+      setWorkingTodoTitle("");
+    }
   }
 
   return (
-    <form 
-    onSubmit={handleAddTodo} >
-      <label htmlFor="todoTitle">Todo</label>
-      <input
-        type="text"
-        id="todoTitle"
-        name="title"
-        ref={todoTitleInput}
-        value= {workingTodoTitle}
-        onChange={(e) => setWorkingTodoTitle(e.target.value)}//controlled input
-       
-        
+    <form onSubmit={handleAddTodo}>
+      <TextInputWithLabel
+        elementId="todoTitle"
+        label="Todo"
+        inputRef={inputRef}
+        value={workingTodoTitle}
+        onChange={(event) => setWorkingTodoTitle(event.target.value)}
       />
-      <button type="submit" disabled={!workingTodoTitle.trim()} >Add Todo</button>
+      <button type="submit" disabled={workingTodoTitle.trim() === ""}>
+        Add Todo
+      </button>
     </form>
   );
 }
