ebc6678a2f123b7d14ba65c63d4a748848fe9089
Niloofar Nayebi <nil_nyb@yahoo.com>
2025-09-08 23:40:21 -0400

    week 7 completed

--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -1,7 +1,12 @@
 import TodoListItem from "./TodoListItem";
 
-function TodoList({ todoList,onCompleteTodo}) {
-     const filteredTodoList = todoList.filter((todo) => !todo.isCompleted);
+function TodoList({ todoList, onCompleteTodo, onUpdateTodo, isLoading }) {
+  const filteredTodoList = todoList.filter((todo) => !todo.isCompleted); // hide completed
+
+  if (isLoading) {
+    return <p>Todo list loading...</p>;
+  }
+
   return (
     <>
       {filteredTodoList.length === 0 ? (
@@ -9,7 +14,12 @@ function TodoList({ todoList,onCompleteTodo}) {
       ) : (
         <ul>
           {filteredTodoList.map((todo) => (
-            <TodoListItem key={todo.id} todo={todo} onCompleteTodo={onCompleteTodo}  />
+            <TodoListItem
+              key={todo.id}
+              todo={todo}
+              onCompleteTodo={onCompleteTodo}
+              onUpdateTodo={onUpdateTodo}
+            />
           ))}
         </ul>
       )}
